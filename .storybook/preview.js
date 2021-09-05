fd0d2fca7f3f4afe525098464b726bbea41a0491
Lucas Eckman <eckm@hey.com>
2021-09-04 23:05:10 -0400

    global styles

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,19 +1,23 @@
-import { ThemeProvider } from "styled-components";
-import { theme } from "../src/theme";
+import { ThemeProvider } from 'styled-components'
+import { theme } from '../src/theme'
+import { GlobalStyles } from '~/global-styles'
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-};
+  viewMode: 'docs',
+  backgrounds: { grid: { cellSize: 16 } },
+}
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <Story />
     </ThemeProvider>
   ),
-];
+]
---
+++ src/global-styles/reset.ts
@@ -0,0 +1,72 @@
+import { css } from 'styled-components'
+
+export const reset = css`
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  body,
+  h1,
+  h2,
+  h3,
+  h4,
+  p,
+  figure,
+  blockquote,
+  dl,
+  dd {
+    margin: 0;
+  }
+
+  ul[role='list'],
+  ol[role='list'] {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  html:focus-within {
+    scroll-behavior: smooth;
+  }
+
+  body {
+    min-height: 100vh;
+    font-family: system-ui, sans-serif;
+    text-rendering: optimizeSpeed;
+  }
+
+  img,
+  picture,
+  svg {
+    display: block;
+    max-width: 100%;
+  }
+
+  *:focus {
+    scroll-margin-bottom: 8vh;
+  }
+
+  input,
+  button,
+  textarea,
+  select {
+    font: inherit;
+  }
+
+  @media (prefers-reduced-motion: reduce) {
+    html:focus-within {
+      scroll-behavior: auto;
+    }
+
+    *,
+    *::before,
+    *::after {
+      transition-duration: 0.01ms !important;
+      animation-duration: 0.01ms !important;
+      animation-iteration-count: 1 !important;
+      scroll-behavior: auto !important;
+    }
+  }
+`

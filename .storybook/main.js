35add54e8ef119734103edccbd215507c9d725cb
Lucas Eckman <eckm@hey.com>
2021-09-04 22:25:23 -0400

    path aliases

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,20 @@
+const path = require('path')
+
 module.exports = {
   stories: [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
-};
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      '~': resolve('../src'),
+    }
+    return config
+  },
+}
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}

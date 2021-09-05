a03d39a78954a3f85ba05c19e6c7252032fc3026
Lucas Eckman <eckm@hey.com>
2021-09-04 23:08:31 -0400

    remove old example stories folder

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  stories: [
-    '../stories/**/*.stories.mdx',
-    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
-    '../src/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   webpackFinal: async (config) => {
     config.resolve.alias = {
